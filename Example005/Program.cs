4d118c23e7245c81e1469a9371e5e5bc7e5cb127
Maria <ixfel363@gmail.com>
2024-02-24 00:05:48 +0300

    Example005 added

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "disk")
+{
+    Console.WriteLine("Hi, Sigma dude");
+}
+
+else
+{
+    Console.Write("Greetings, ");
+    Console.Write(username);
+}
